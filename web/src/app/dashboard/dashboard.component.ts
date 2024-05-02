ba08cdb939cc087064b35b2aaf196c24a3d395d9
Maxime Vincent <git@maximevincent.com>
2024-05-02 17:31:48 -0400

    display real data in dashboard using data service

--- web/src/app/dashboard/dashboard.component.ts
+++ web/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Device } from '../model/device.model';
+import {Component, OnInit} from '@angular/core';
+import {Device} from '../model/device.model';
 
-import { NzUploadFile } from 'ng-zorro-antd/upload';
+import {NzUploadFile} from 'ng-zorro-antd/upload';
+import {DataService} from "../../services/data/data.service";
+import {da_DK} from "ng-zorro-antd/i18n";
 
 const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
   new Promise((resolve, reject) => {
@@ -12,39 +14,53 @@ const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
   });
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.scss'
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
 })
 
-export class DashBoardComponent implements OnInit{
-    
-    listOfData: Device[] = [
-        new Device(1, 'Rapsberry Pi Model 4 B', false)
-    ];
+export class DashBoardComponent implements OnInit {
+  listOfData: Device[] = [];
 
-    fileList: NzUploadFile[] = [
-        // {
-        //   uid: '-1',
-        //   name: 'image.png',
-        //   status: 'done',
-        //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-        // }
-      ];
-      previewImage: string | undefined = '';
-      previewVisible = false;
-    
-      handlePreview = async (file: NzUploadFile): Promise<void> => {
-        if (!file.url && !file.preview) {
-          file.preview = await getBase64(file.originFileObj!);
-        }
-        this.previewImage = file.url || file.preview;
-        this.previewVisible = true;
-      };
+  fileList: NzUploadFile[] = [
+    // {
+    //   uid: '-1',
+    //   name: 'image.png',
+    //   status: 'done',
+    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    // }
+  ];
+  previewImage: string | undefined = '';
+  previewVisible = false;
 
-    constructor() {}
-    
-    ngOnInit(): void {
-        
+  handlePreview = async (file: NzUploadFile): Promise<void> => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj!);
     }
+    this.previewImage = file.url || file.preview;
+    this.previewVisible = true;
+  };
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.dataService.getData("/devices")
+      .subscribe(data => {
+        let deviceList: Device[] = [];
+
+        data.forEach((device: any) => {
+          let id: number = device.id;
+          let name: string = device.name;
+          let location: string = device.location;
+          let isLost: boolean = device.lost;
+
+          deviceList.push(new Device(id, name, location, isLost));
+        })
+        this.listOfData = deviceList;
+      });
+  }
 }
--- web/src/app/model/device.model.ts
+++ web/src/app/model/device.model.ts
@@ -1,11 +1,13 @@
 export class Device {
-    id: number;
-    name: string;
-    isLocated: boolean;
-    
-    constructor(id: number, name: string, isLocated: boolean) {
-        this.id = id;
-        this.name = name;
-        this.isLocated = isLocated;
-    }
+  id: number;
+  name: string;
+  location: string;
+  isLost: boolean;
+
+  constructor(id: number, name: string, location: string, isLost: boolean) {
+    this.id = id;
+    this.name = name;
+    this.location = location;
+    this.isLost = isLost;
+  }
 }
