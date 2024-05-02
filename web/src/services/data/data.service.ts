925e55fa6a185dd1654576d7d56904c2313d6cc9
Maxime Vincent <git@maximevincent.com>
2024-05-02 17:31:22 -0400

    add data service

---
+++ web/src/services/data/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getData(path: String): Observable<any> {
+    return this.http.get<any>(this.apiUrl + path);
+  }
+
+  postData(path: String, data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl + path, data);
+  }
+}
