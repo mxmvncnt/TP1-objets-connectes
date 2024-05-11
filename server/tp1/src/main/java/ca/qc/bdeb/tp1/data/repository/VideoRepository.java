219664386d31627e5206d3ab0001266c56639021
Maxime Vincent <git@maximevincent.com>
2024-05-10 23:37:18 -0400

    add history handling

--- server/tp1/src/main/java/ca/qc/bdeb/tp1/data/repository/VideoRepository.java
+++ server/tp1/src/main/java/ca/qc/bdeb/tp1/data/repository/VideoRepository.java
@@ -3,8 +3,10 @@ package ca.qc.bdeb.tp1.data.repository;
 import ca.qc.bdeb.tp1.data.entity.Device;
 import ca.qc.bdeb.tp1.data.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface VideoRepository extends JpaRepository<Video, Integer>{
-}
+public interface VideoRepository extends JpaRepository<Video, Integer>{ }
