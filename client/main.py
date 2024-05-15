25e5a3391c58cb49a5eae448ee15def715c304a3
Maxime Vincent <git@maximevincent.com>
2024-05-14 20:58:56 -0400

    [client] fix clock not starting when no videos

--- client/main.py
+++ client/main.py
@@ -8,14 +8,6 @@ from video_controller import VideoController
 play_list = PlayList()
 
 
-def main():
-    if play_list.videos_exist():
-        th_video_controller.start()
-        th_video_controller.join()
-    else:
-        th_clock.start()
-
-
 def start_clock():
     app = ClockApp(tk.Tk())
 
@@ -29,5 +21,10 @@ def start_video_controller():
 if __name__ == '__main__':
     th_video_controller = threading.Thread(target=start_video_controller, args=(), daemon=True)
     th_clock = threading.Thread(target=start_clock, args=(), daemon=True)
-    
-    main()
+
+    if play_list.videos_exist():
+        th_video_controller.start()
+        th_video_controller.join()
+    else:
+        th_clock.start()
+        th_clock.join()
