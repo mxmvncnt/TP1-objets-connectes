3238d2cda8a03912c723c54c0a5c74db467b41df
Maxime Vincent <git@maximevincent.com>
2024-05-14 23:27:11 -0400

    Update video_utils.py

--- client/video_utils.py
+++ client/video_utils.py
@@ -1,6 +1,5 @@
 import json
 import os
-from typing import List
 
 import settings as s
 
@@ -52,6 +51,7 @@ def delete_local_watch_data():
 
 
 def download_video(video: Video):
+    print(video)
     print(f"Downloading: {video.fichier}")
 
     missing_video = requests.get(
@@ -89,7 +89,7 @@ def remove_video_from_playlist(video: Video):
 
 
 # Get the videos that the local device is missing
-def get_missing_videos(server_videos: list[Video], local_videos: list[Video]):
+def get_missing_videos(server_videos: list[Video], local_videos: list[Video]) -> list[Video]:
     missing_videos = []
     for server_video in server_videos:
         if server_video not in local_videos:
@@ -99,10 +99,60 @@ def get_missing_videos(server_videos: list[Video], local_videos: list[Video]):
 
 
 # Get the videos that are present on the device but not on the server
-def get_incorrect_videos(server_videos: list[Video], local_videos: list[Video]):
+def get_incorrect_videos(server_videos: list[Video], local_videos: list[Video]) -> list[Video]:
     incorrect_videos = []
     for local_video in local_videos:
         if local_video not in server_videos:
             incorrect_videos.append(local_video)
 
     return incorrect_videos
+
+
+def get_server_videos() -> list[Video]:
+    print("Getting server videos...")
+    server_videos = requests.get(
+        url=f"{os.getenv('SERVER_URL')}/devices/{s.DEVICE_ID}/playlist",
+        headers=headers
+    )
+
+    server_videos_json = json.loads(server_videos.content)
+    return get_video_objects_from_json(server_videos_json)
+
+
+def get_local_videos() -> list[Video]:
+    videos_db_response = requests.get(f"{os.getenv('API_URL')}/video/list")
+    videos_db_json = videos_db_response.json()
+
+    if not isinstance(videos_db_json, list):
+        return []
+
+    return [Video(**data) for data in videos_db_json]
+
+
+def add_missing_videos():
+    print("Getting missing videos...")
+    missing_videos = get_missing_videos(get_server_videos(), get_local_videos())
+
+    for missing_video in missing_videos:
+        download_video(missing_video)
+        add_video_to_playlist(missing_video)
+        print("Done.")
+
+
+def get_video_objects_from_json(json_data) -> list[Video]:
+    videos_objets = []
+
+    print(json_data)
+
+    for video in json_data:
+        new_video = Video(
+            id=video.get('id'),
+            fichier=video.get('file'),
+            taille=video.get('size'),
+            ordre=1,
+            md5=video.get('md5')
+        )
+
+        videos_objets.append(new_video)
+
+    return videos_objets
